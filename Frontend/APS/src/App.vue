a0069f2ec7a0f0583526edbf7f1472a210f6cfdf
Ebrima S Jallow <100509637+C-Jalloh@users.noreply.github.com>
2024-12-04 03:18:33 +0000

    Add files via upload

---
+++ Frontend/APS/src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import ThemoviePage from './components/ThemoviePage.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Two people moviening" class="logo" src="./assets/moviening1.png" width="125" height="125" />
+
+    <div class="wrapper">
+      <ThemoviePage movie="moviening is the best" />
+    </div>
+    
+  </header>
+
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
