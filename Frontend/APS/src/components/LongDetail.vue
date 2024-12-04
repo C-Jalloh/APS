55407063d6fe42b8691c70b340aba6b07d6f74ae
Ebrima S Jallow <100509637+C-Jalloh@users.noreply.github.com>
2024-12-04 03:16:23 +0000

    Add files via upload

---
+++ Frontend/APS/src/components/LongDetail.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+
+    <template>
+
+        <div>
+            <img class="img" src="./assets/moviening1.png" >
+            <h4 class="title"></h4>
+            
+
+        </div>
+    </template>
+
+
+<style scoped>
+.img {
+width: 100;
+height: 100}
+h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
+}
+</style>
---
+++ Frontend/APS/src/components/RatingIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-rating
+  background-color="grey"
+  color="green"
+  empty-icon="$mdiStarOutline"
+  full-icon="$mdiStar"
+  half-icon="$mdiStarHalfFull"
+  hover
+  length="5"
+  size="64"
+  value="3"
+></v-rating>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
